31282144a4add23a6bba1b8e740dcfd201816434
David Clausen <davidclausen2051@gmail.com>
2019-06-16 09:55:47 -0500

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
 import express from 'express';
 import jwt from 'express-jwt';
 import 'reflect-metadata';
@@ -11,6 +12,8 @@ import { schema } from './graphql';
 
 async function startServer() {
   const app = express();
+
+  app.use(cors());
 
   app.use(
     jwt({
