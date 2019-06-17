3ab02a7954ca5fd9f6d285c63ac214c6e87eb6af
David Clausen <davidclausen2051@gmail.com>
2019-06-16 22:25:33 -0500

    debug token print

--- src/emails/sendTokenEmail.ts
+++ src/emails/sendTokenEmail.ts
@@ -5,6 +5,12 @@ import { getMailer } from './getMailer';
 export async function sendTokenEmail(user: User, token: string, template: 'reset' | 'confirmation') {
   const email = getMailer();
 
+  console.log('------------EMAIL-----------');
+  console.log(`----${template} TOKEN------`);
+  console.log(`${token}`);
+  console.log('------------TOKEN-----------');
+  console.log('-------------END--------------');
+
   try {
     await email.send({
       template,
